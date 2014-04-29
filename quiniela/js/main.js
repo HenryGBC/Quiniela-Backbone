f3f9411fae40e2f3cbe2009800e9c69aca773346
HenryGBC <henry.bravo89@gmail.com>
2014-04-29 10:53:57 -0430

    First commit, alls the archives of Rbenginnering/quiniela

---
+++ quiniela/js/main.js
@@ -0,0 +1,48 @@
+$(function(){
+	$("#btnTableA").click(function(){
+		
+
+	  $("#positionsA").toggle("slow");
+	});
+	$("#btnTableB").click(function(){
+
+
+	  $("#positionsB").toggle("slow");
+	});
+	$("#btnTableC").click(function(){
+		
+
+	  $("#positionsC").toggle("slow");
+	});
+	$("#btnTableD").click(function(){
+
+
+	  $("#positionsD").toggle("slow");
+	});
+	$("#btnTableE").click(function(){
+		
+
+	  $("#positionsE").toggle("slow");
+	});
+	$("#btnTableF").click(function(){
+
+
+	  $("#positionsF").toggle("slow");
+	});
+	$("#btnTableG").click(function(){
+		
+
+	  $("#positionsG").toggle("slow");
+	});
+	$("#btnTableH").click(function(){
+
+
+	  $("#positionsH").toggle("slow");
+	});
+
+	$(".icon-remove").click(function(){
+	
+	
+	  $(".alert-table").fadeOut();
+	});
+});
